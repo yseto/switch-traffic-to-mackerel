782043f48b8bd71bee8adc9fc32f1bb4efff22a6
yseto <yseto@users.noreply.github.com>
2023-03-14 16:48:47 +0000

    create config package. and sample

---
+++ config/config.go
@@ -0,0 +1,21 @@
+package config
+
+type Config struct {
+	Community    string     `yaml:"community"`
+	Target       string     `yaml:"target"`
+	Interface    *Interface `yaml:"interface"`
+	Mibs         []string   `yaml:"mibs"`
+	SkipLinkdown bool       `yaml:"skip-linkdown"`
+	Name         string     `yaml:"name"`
+	Mackerel     *Mackerel  `yaml:"mackerel"`
+}
+
+type Interface struct {
+	Include string `yaml:"include"`
+	Exclude string `yaml:"exclude"`
+}
+
+type Mackerel struct {
+	HostID string `yaml:"host-id"`
+	ApiKey string `yaml:"x-api-key"`
+}

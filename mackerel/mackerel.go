02f958a007b7b9cf8c13b890ccbfdf873c18869d
yseto <yseto@users.noreply.github.com>
2023-03-15 12:44:19 +0000

    cleanup

--- mackerel/mackerel.go
+++ mackerel/mackerel.go
@@ -3,11 +3,8 @@ package mackerel
 import (
 	"container/list"
 	"context"
-	"fmt"
 	"log"
-	"math"
 	"os"
-	"strings"
 	"sync"
 	"time"
 
@@ -20,31 +17,6 @@ import (
 var buffers = list.New()
 var mutex = &sync.Mutex{}
 var snapshot []collector.MetricsDutum
-
-var overflowValue = map[string]uint64{
-	"ifInOctets":    math.MaxUint32,
-	"ifOutOctets":   math.MaxUint32,
-	"ifHCInOctets":  math.MaxUint64,
-	"ifHCOutOctets": math.MaxUint64,
-	"ifInDiscards":  math.MaxUint64,
-	"ifOutDiscards": math.MaxUint64,
-	"ifInErrors":    math.MaxUint64,
-	"ifOutErrors":   math.MaxUint64,
-}
-
-var receiveDirection = map[string]bool{
-	"ifInOctets":   true,
-	"ifHCInOctets": true,
-	"ifInDiscards": true,
-	"ifInErrors":   true,
-}
-
-var deltaValues = map[string]bool{
-	"ifInOctets":    true,
-	"ifOutOctets":   true,
-	"ifHCInOctets":  true,
-	"ifHCOutOctets": true,
-}
 
 func initialForMackerel(c *config.Config, client *mackerel.Client) (*string, error) {
 	log.Println("init for mackerel")
@@ -91,60 +63,6 @@ func initialForMackerel(c *config.Config, client *mackerel.Client) (*string, err
 		return nil, err
 	}
 	return &hostId, nil
-}
-
-func escapeInterfaceName(ifName string) string {
-	return strings.Replace(strings.Replace(strings.Replace(ifName, "/", "-", -1), ".", "_", -1), " ", "", -1)
-}
-
-func calcurateDiff(a, b, overflow uint64) uint64 {
-	if b < a {
-		return overflow - a + b
-	} else {
-		return b - a
-	}
-}
-
-func Enqueue(rawMetrics []collector.MetricsDutum) {
-	prevSnapshot := snapshot
-	snapshot = rawMetrics
-
-	now := time.Now().Unix()
-
-	metrics := make([]*mackerel.MetricValue, 0)
-	for _, metric := range rawMetrics {
-		prevValue := metric.Value
-		for _, v := range prevSnapshot {
-			if v.IfIndex == metric.IfIndex && v.Mib == metric.Mib {
-				prevValue = v.Value
-				break
-			}
-		}
-
-		value := calcurateDiff(prevValue, metric.Value, overflowValue[metric.Mib])
-
-		var name string
-		ifName := escapeInterfaceName(metric.IfName)
-		if deltaValues[metric.Mib] {
-			direction := "txBytes"
-			if receiveDirection[metric.Mib] {
-				direction = "rxBytes"
-			}
-			name = fmt.Sprintf("interface.%s.%s.delta", ifName, direction)
-			value /= 60
-		} else {
-			name = fmt.Sprintf("custom.interface.%s.%s", metric.Mib, ifName)
-		}
-		metrics = append(metrics, &mackerel.MetricValue{
-			Name:  name,
-			Time:  now,
-			Value: value,
-		})
-	}
-
-	mutex.Lock()
-	buffers.PushBack(metrics)
-	mutex.Unlock()
 }
 
 func sendTicker(ctx context.Context, wg *sync.WaitGroup, client *mackerel.Client, hostId *string) {
